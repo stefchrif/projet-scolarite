8669edced6d757e8ad7b670faabb8caae41578f2
mustaphaelatrassi <mustapha.elat@gmail.com>
2026-05-16 15:40:12 +0100

    feat: classe principale de l'application

---
+++ backend/src/main/java/ma/uit/scolarite/ScolariteApplication.java
@@ -0,0 +1,15 @@
+package ma.uit.scolarite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Point d'entree de l'application de gestion de scolarite.
+ */
+@SpringBootApplication
+public class ScolariteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScolariteApplication.class, args);
+    }
+}
