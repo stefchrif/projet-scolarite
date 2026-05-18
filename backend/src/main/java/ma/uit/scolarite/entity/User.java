4ff957b894bac42e00f79db72afb3739f9003d12
mustaphaelatrassi <mustapha.elat@gmail.com>
2026-05-18 11:05:44 +0100

    feat(entity): entite User pour l'authentification

---
+++ backend/src/main/java/ma/uit/scolarite/entity/User.java
@@ -0,0 +1,38 @@
+package ma.uit.scolarite.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Compte utilisateur (authentification). Chaque utilisateur possede un role.
+ */
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nomComplet;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
