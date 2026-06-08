6add1b1aec02de76e06bcb19e4e12c8a16bc189b
mustaphaelatrassi <mustapha.elat@gmail.com>
2026-06-08 13:50:22 +0100

    feat(repository): repositories User et Filiere

---
+++ backend/src/main/java/ma/uit/scolarite/repository/FiliereRepository.java
@@ -0,0 +1,7 @@
+package ma.uit.scolarite.repository;
+
+import ma.uit.scolarite.entity.Filiere;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FiliereRepository extends JpaRepository<Filiere, Long> {
+}
